3e82f45c86afa3604ba3abd6a4dc5433e2d6ec2d
Laurent <biguphpc@gmail.com>
2014-02-13 16:39:31 -0600

    Create simple JSP project example, with multi module maven project, and standalone executable jar generation with shade plugin.

---
+++ jsp-standalone-example/jetty-boostrap-runner/src/main/java/org/teknux/jb/examples/vaadin/Main.java
@@ -0,0 +1,29 @@
+package org.teknux.jb.examples.vaadin;
+
+import org.teknux.jettybootstrap.JettyBootstrap;
+import org.teknux.jettybootstrap.JettyBootstrapException;
+
+
+public class Main {
+
+	/**
+	 * @see JettyBootstrap.startSelf()
+	 */
+	public static void main(String[] args) {
+		try {
+			// create the jetty boostrap object
+			JettyBootstrap jBootstrap = new JettyBootstrap();
+			// we are embedding the war inside our jar under the "webapp" directory with Maven
+			jBootstrap.addExplodedWarAppFromClasspath("/webapp");
+			// start the server
+			jBootstrap.startServer();
+
+			// All those lines can be replaced with JettyBootstrap.startSelf()
+			
+		} catch (JettyBootstrapException e) {
+			e.printStackTrace(); // bad exception handling !
+			System.exit(1);
+		}
+	}
+
+}
